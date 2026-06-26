614a0692f8a8e1cd715d18cca24468f6b6eb4ccb
jordidy <41534439+jordidy@users.noreply.github.com>
2026-06-26 08:14:35 +0000

    change

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export JUPYTER_TOKEN=''
-
 jupyter kernelgateway \
     --ip=0.0.0.0 \
     --port=${PORT:-8080} \
-    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket
+    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket \
+    --KernelGatewayApp.auth_token='' \
+    --KernelGatewayApp.allow_origin='*'
