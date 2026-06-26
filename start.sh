eb86df5702f6bff3aeed0614a34b91afedec0be7
jordidy <41534439+jordidy@users.noreply.github.com>
2026-06-26 08:03:39 +0000

    add secret

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 jupyter kernelgateway \
     --ip=0.0.0.0 \
     --port=${PORT:-8080} \
-    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket
+    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket \
+    --KernelGatewayApp.auth_token=${JUPYTER_TOKEN}
