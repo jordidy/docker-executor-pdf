60873721ff7a6f1a946b7489f4ae7f0ebe945b8e
jordidy <41534439+jordidy@users.noreply.github.com>
2026-06-26 08:08:10 +0000

    change token

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ jupyter kernelgateway \
     --ip=0.0.0.0 \
     --port=${PORT:-8080} \
     --KernelGatewayApp.api=kernel_gateway.jupyter_websocket \
-    --KernelGatewayApp.auth_token=${JUPYTER_TOKEN}
+    --KernelGatewayApp.auth_token=''
