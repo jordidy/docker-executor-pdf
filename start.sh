6a3eb881909121d232c36c40dd93ddb047c3d619
jordidy <41534439+jordidy@users.noreply.github.com>
2026-06-26 08:12:40 +0000

    dd

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export JUPYTER_TOKEN=''
+
 jupyter kernelgateway \
     --ip=0.0.0.0 \
     --port=${PORT:-8080} \
-    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket \
-    --KernelGatewayApp.auth_token=${JUPYTER_TOKEN}
+    --KernelGatewayApp.api=kernel_gateway.jupyter_websocket
